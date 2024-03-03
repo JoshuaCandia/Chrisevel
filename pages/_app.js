6266e5bbde385102bbba0ca66f71ebb4df7d11b7
Joshua Candia <joshuacandia74@gmail.com>
2024-03-02 21:51:51 -0300

    Add PopupWidget component to _app.js and remove PopupWidget component from index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
 import Navbar from "../components/navbar";
+import PopupWidget from "../components/popupWidget";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Navbar />
       <Component {...pageProps} />
+      <PopupWidget />
     </ThemeProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -58,7 +58,6 @@ const Home = () => {
       <Faq />
       <Cta />
       <Footer />
-      <PopupWidget />
     </>
   );
 };
