09bae0aa554dc1ba23ffb21382f561f6e3d30d44
Joshua Candia <joshuacandia74@gmail.com>
2024-08-25 15:22:42 -0300

    refactor: Update Card component styles, image handling, and improve performance with lazy loading

--- pages/contacto/index.js
+++ pages/contacto/index.js
@@ -3,34 +3,34 @@ import Footer from "../../components/footer";
 
 const index = () => {
   return (
-    <section class="bg-blue-50 dark:bg-slate-800" id="contact">
-      <div class="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
-        <div class="mb-4">
-          <div class="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
-            <h2 class="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
+    <section className="" id="contact">
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20">
+        <div className="mb-4">
+          <div className="mb-6 max-w-3xl text-center sm:text-center md:mx-auto md:mb-12">
+            <h2 className="font-heading mb-4 font-bold tracking-tight text-gray-900 dark:text-white text-3xl sm:text-5xl">
               Contacto
             </h2>
-            <p class="mx-auto mt-4 max-w-3xl text-xl text-gray-600 dark:text-slate-400">
+            <p className="mx-auto mt-4 max-w-3xl text-xl text-gray-600 dark:text-slate-400">
               ¿Tienes alguna pregunta o comentario? , no dudes en contactarnos
             </p>
           </div>
         </div>
-        <div class="flex items-stretch justify-center">
-          <div class="grid md:grid-cols-2">
-            <div class="h-full pr-6">
-              <p class="text-base font-semibold uppercase tracking-wide text-text-blue-600 dark:text-blue-200">
+        <div className="flex items-stretch justify-center">
+          <div className="grid md:grid-cols-2">
+            <div className="h-full pr-6">
+              <p className="text-base font-semibold uppercase tracking-wide text-text-blue-600 dark:text-blue-200">
                 Enchufando
               </p>
-              <p class="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
+              <p className="mt-3 mb-12 text-lg text-gray-600 dark:text-slate-400">
                 Es una empresa nacida en el 2006, dedicada a la fabricación y
                 comercialización de productos petroquímicos a través de
                 rigurosos protocolos que llevan a una productividad eficiente
                 generando negocios superiores dentro de un marco empresarial
                 serio y responsable.
               </p>
-              <ul class="mb-6 md:mb-0">
-                <li class="flex">
-                  <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+              <ul className="mb-6 md:mb-0">
+                <li className="flex">
+                  <div className="flex h-10 w-10 items-center justify-center rounded text-gray-50">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -41,26 +41,26 @@ const index = () => {
                       stroke-width="2"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                     >
                       <path d="M9 11a3 3 0 1 0 6 0a3 3 0 0 0 -6 0"></path>
                       <path d="M17.657 16.657l-4.243 4.243a2 2 0 0 1 -2.827 0l-4.244 -4.243a8 8 0 1 1 11.314 0z"></path>
                     </svg>
                   </div>
-                  <div class="ml-4 mb-4">
-                    <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                  <div className="ml-4 mb-4">
+                    <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
                       Nuestra dirección
                     </h3>
-                    <p class="text-gray-600 dark:text-slate-400">
+                    <p className="text-gray-600 dark:text-slate-400">
                       Washington 496, General Pacheco
                     </p>
-                    <p class="text-gray-600 dark:text-slate-400">
+                    <p className="text-gray-600 dark:text-slate-400">
                       Buenos Aires, Argentina
                     </p>
                   </div>
                 </li>
-                <li class="flex">
-                  <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                <li className="flex">
+                  <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -71,31 +71,31 @@ const index = () => {
                       stroke-width="2"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                     >
                       <path d="M5 4h4l2 5l-2.5 1.5a11 11 0 0 0 5 5l1.5 -2.5l5 2v4a2 2 0 0 1 -2 2a16 16 0 0 1 -15 -15a2 2 0 0 1 2 -2"></path>
                       <path d="M15 7a2 2 0 0 1 2 2"></path>
                       <path d="M15 3a6 6 0 0 1 6 6"></path>
                     </svg>
                   </div>
-                  <div class="ml-4 mb-4">
-                    <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                  <div className="ml-4 mb-4">
+                    <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
                       Medios de contacto
                     </h3>
                     <a href="tel:+541156161163">
-                      <p class="text-gray-600 dark:text-slate-400">
+                      <p className="text-gray-600 dark:text-slate-400">
                         Teléfono: +54 9 11 5616-1163
                       </p>
                     </a>
                     <a href="mailto:info@enchufando.com">
-                      <p class="text-gray-600 dark:text-slate-400">
+                      <p className="text-gray-600 dark:text-slate-400">
                         Mail: info@enchufando.com
                       </p>
                     </a>
                   </div>
                 </li>
-                <li class="flex">
-                  <div class="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
+                <li className="flex">
+                  <div className="flex h-10 w-10 items-center justify-center rounded bg-blue-900 text-gray-50">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="24"
@@ -106,66 +106,66 @@ const index = () => {
                       stroke-width="2"
                       stroke-linecap="round"
                       stroke-linejoin="round"
-                      class="h-6 w-6"
+                      className="h-6 w-6"
                     >
                       <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
                       <path d="M12 7v5l3 3"></path>
                     </svg>
                   </div>
-                  <div class="ml-4 mb-4">
-                    <h3 class="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
+                  <div className="ml-4 mb-4">
+                    <h3 className="mb-2 text-lg font-medium leading-6 text-gray-900 dark:text-white">
                       Horario de atención
                     </h3>
-                    <p class="text-gray-600 dark:text-slate-400">
+                    <p className="text-gray-600 dark:text-slate-400">
                       Lunes a Viernes: 08:00 - 18:00
                     </p>
-                    <p class="text-gray-600 dark:text-slate-400">
+                    <p className="text-gray-600 dark:text-slate-400">
                       Sábados, Domingos y Feriados: Cerrado
                     </p>
                   </div>
                 </li>
               </ul>
             </div>
-            <div class="card h-fit max-w-6xl p-5 md:p-12" id="form">
-              <h2 class="mb-4 text-2xl font-bold dark:text-white">
+            <div className="card h-fit max-w-6xl p-5 md:p-12" id="form">
+              <h2 className="mb-4 text-2xl font-bold dark:text-white">
                 Envíanos un mensaje
               </h2>
               <form id="contactForm">
-                <div class="mb-6">
-                  <div class="mx-0 mb-1 sm:mb-4">
-                    <div class="mx-0 mb-1 sm:mb-4">
+                <div className="mb-6">
+                  <div className="mx-0 mb-1 sm:mb-4">
+                    <div className="mx-0 mb-1 sm:mb-4">
                       <label
                         for="name"
-                        class="pb-1 text-xs uppercase tracking-wider"
+                        className="pb-1 text-xs uppercase tracking-wider"
                       />
                       <input
                         type="text"
                         id="name"
                         autocomplete="given-name"
                         placeholder="Tu nombre"
-                        class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                        className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
                         name="name"
                       />
                     </div>
-                    <div class="mx-0 mb-1 sm:mb-4">
+                    <div className="mx-0 mb-1 sm:mb-4">
                       <label
                         for="email"
-                        class="pb-1 text-xs uppercase tracking-wider"
+                        className="pb-1 text-xs uppercase tracking-wider"
                       ></label>
                       <input
                         type="email"
                         id="email"
                         autocomplete="email"
                         placeholder="Tu dirección de correo electrónico"
-                        class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                        className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
                         name="email"
                       />
                     </div>
                   </div>
-                  <div class="mx-0 mb-1 sm:mb-4">
+                  <div className="mx-0 mb-1 sm:mb-4">
                     <label
                       for="textarea"
-                      class="pb-1 text-xs uppercase tracking-wider"
+                      className="pb-1 text-xs uppercase tracking-wider"
                     ></label>
                     <textarea
                       id="textarea"
@@ -173,14 +173,14 @@ const index = () => {
                       cols="30"
                       rows="5"
                       placeholder="Escribe tu mensaje.."
-                      class="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+                      className="mb-2 w-full rounded-md border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
                     ></textarea>
                   </div>
                 </div>
-                <div class="text-center">
+                <div className="text-center">
                   <button
                     type="submit"
-                    class="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0"
+                    className="w-full bg-blue-800 text-white px-6 py-3 font-xl rounded-md sm:mb-0"
                   >
                     Enviar
                   </button>
